cfa53c1498416351b0ddb2b447c998ff43bb5cf8
Farhan Murtaza <farhanmurtza03@gmail.com>
2025-06-11 22:48:52 +0500

    Adding some basic authentication using clerk

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-medium text-sky-700">Hello World</p>
-      <Button variant="destructive">Click me</Button>
+      <UserButton afterSwitchSessionUrl="/" />
     </div>
   );
 }
